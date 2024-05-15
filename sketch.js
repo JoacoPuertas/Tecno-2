b04ba8b0abe62aba008e2f8d2e77805360f0d450
mateodiazw <mateodiazw@gmail.com>
2024-05-15 20:51:30 -0300

    Zenon

--- sketch.js
+++ sketch.js
@@ -1,32 +1,31 @@
-//declaro un arreglo
-let c = [];
-let cantidad = 10;
+let lineasVerticales = [];
+let posX = 0;
+let posY; 
 
-function setup() {
-  createCanvas( windowWidth, windowHeight );
-  
-  for( let i=0 ; i<cantidad ; i++ ){
-    c[i] = new Caminante( width/2 , height/2 );
+function preload(){
+
+  //Las imagenes del 0 a 6 son las calidas y las del 6 al 10 son frias.
+  for(let i = 0; i < 1; i++) {
+    lineasVerticales[i] = loadImage("data/vertical" + i + ".png");
   }
-
-  background(255);
-  fill(0);
 }
 
+function setup() {
+  createCanvas( 500, 500 );
+  posY = 0;
+}
+
+
+
 function draw() {
-  /*push();
-  noStroke()
-  fill(255,5);
-  rect( 0 , 0 , width , height );
-  pop();*/
-
-  for( let i=0 ; i<cantidad ; i++ ){
-
-    /*if( mouseIsPressed ){
-      c[i].seguir( mouseX , mouseY );
-    }*/
-    c[i].actualizar();
-    c[i].dibujar();
-    
-  }
+  background(156, 203, 241);
+  
+   for(let i = 0; i < 6; i++){
+    posY = random(-100, 20);
+    image(lineasVerticales[0], posX + i * 80 , posY, 250, 250);
+  } 
+  posY++;
+  fill(0);
+  textSize(40);
+  text(posX, 50, 50)
 }
