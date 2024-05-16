4c82689adc45df297b83f1d826a5cba642bc1e53
LaraPuppo <larapuppomugetti@gmail.com>
2024-05-16 13:47:16 -0300

    x

--- sketch.js
+++ sketch.js
@@ -3,20 +3,21 @@ let cantidadVerticales = 9;
 function preload(){
 
   //Las imagenes del 0 a 6 son las calidas y las del 6 al 10 son frias.
-  for(let i = 0; i < cantidadVerticales; i++) {
+  for(let i = 0; i <= cantidadVerticales; i++) {
     lineasVerticales[i] = loadImage("data/vertical" + i + ".png");
   }
 }
 
 function setup() {
   imageMode(CENTER);
-  createCanvas( windowHeight, windowHeight );
+  createCanvas( 500, 500 );
   c = new vertical(lineasVerticales);
+  background(156, 203, 241);
 }
 
 
 
 function draw() {
-  background(156, 203, 241);
+ 
   c.dibujar();
 }
--- vertical.js
+++ vertical.js
@@ -2,14 +2,28 @@ class vertical {
     constructor(vertical){
         this.img = [];
         this.img = vertical;
-        this.cantidadVerticales = 9;
+        this.cantidadVerticales = random (3,5);
+        this.estanDibujadas= false ;
     }
 
     dibujar(){
+    
+        //crear un for con un arreglo de 5 posiciones y guardar en cada posicion del arreglo un valor 
+        //random para pintar desp. Limitarlo con una booleana para q se reproduzca solo el primer frame
 
-        for(let i = 0; i <= this.cantidadVerticales; i++) {
-            image(this.img[i], 100 + i * 50 , height/2, 350, 350);
-        }
         
+if (!this.estanDibujadas){
+    
+    for (let i = 0; i < this.cantidadVerticales; i++) {
+            if (this.img[i]) {
+                this.pinceladaRandom= int(random(9));
+                image(this.img[this.pinceladaRandom], 100 + i * 75 , height/2, 350, 350);
+            }
+        }
+}
+this.estanDibujadas = true;
+
     }
+
+    
 }
