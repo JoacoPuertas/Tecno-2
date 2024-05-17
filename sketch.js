fe7ea5e25e41b1a3771863fde093999137507f93
JoacoPuertas <joacopuertas3@gmail.com>
2024-05-16 23:43:38 -0300

    probando cosas que no salieron

--- sketch.js
+++ sketch.js
@@ -1,23 +1,24 @@
 let lineasVerticales = [];
 let cantidadVerticales = 9;
-function preload(){
+let c;
+// let posY = 0;
 
-  //Las imagenes del 0 a 6 son las calidas y las del 6 al 10 son frias.
-  for(let i = 0; i <= cantidadVerticales; i++) {
-    lineasVerticales[i] = loadImage("data/vertical" + i + ".png");
-  }
+function preload() {
+    // Cargar las imágenes
+    for (let i = 0; i <= cantidadVerticales; i++) {
+        lineasVerticales[i] = loadImage("data/vertical" + i + ".png");
+    }
 }
 
 function setup() {
-  imageMode(CENTER);
-  createCanvas( 500, 500 );
-  c = new vertical(lineasVerticales);
-  background(156, 203, 241);
+    imageMode(CENTER);
+    createCanvas(500, 500);
+    c = new vertical(lineasVerticales);
+    c.inicializar();
+    background(156, 203, 241);
 }
 
-
-
 function draw() {
- 
-  c.dibujar();
+    c.dibujar();
+    c.actualizar();
 }
--- vertical.js
+++ vertical.js
@@ -1,29 +1,52 @@
+
 class vertical {
-    constructor(vertical){
-        this.img = [];
+    constructor(vertical) {
         this.img = vertical;
-        this.cantidadVerticales = random (3,5);
-        this.estanDibujadas= false ;
+        this.cantidadVerticales = int(random(3, 7)); // Asegurar que es un número entero
+        this.estanDibujadas = false;
+        this.posY = [];
+        this.direccion = [];
+        this.velocidad = 5;
     }
 
-    dibujar(){
-    
-        //crear un for con un arreglo de 5 posiciones y guardar en cada posicion del arreglo un valor 
-        //random para pintar desp. Limitarlo con una booleana para q se reproduzca solo el primer frame
+    inicializar() {
+        for (let i = 0; i < this.cantidadVerticales; i++) {
+            this.posY.push(random(-50, height + 50));
+            this.direccion.push(i < 6 ? 1 : -1); // Direcciones: 1 para abajo, -1 para arriba
+        }
+    }
 
-        
-if (!this.estanDibujadas){
-    
-    for (let i = 0; i < this.cantidadVerticales; i++) {
-            if (this.img[i]) {
-                this.pinceladaRandom= int(random(9));
-                image(this.img[this.pinceladaRandom], 100 + i * 75 , height/2, 350, 350);
+    dibujar() {
+        if (!this.estanDibujadas) {
+            for (let i = 0; i < this.cantidadVerticales; i++) {
+                let idx = int(random(this.img.length)); // Índice aleatorio dentro del rango de imágenes
+                if (this.img[idx]) {
+                    let x = 100 + i * 75;
+                    let y = this.posY[i]; // Usar i para mantener la posición correcta
+                    image(this.img[idx], x, y, 350, 350);
+                }
+            }
+            this.estanDibujadas = true;
+        }
+        // if (!this.estanDibujadas){
+        //     for (let i = 0; i < this.cantidadVerticales; i++) {
+        //         if (this.img[i]) {
+        //             this.pinceladaRandom= int(random(9));
+        //             image(this.img[this.pinceladaRandom], 100 + i * 75 , this.posY[i], 350, 350);
+        //         }
+        //     }
+        // }
+    }
+
+    actualizar() {
+        for (let i = 0; i < this.posY.length; i++) {
+            this.posY[i] += this.velocidad * this.direccion[i];
+            // Reiniciar la posición si la imagen se sale de los límites
+            if (this.direccion[i] === 1 && this.posY[i] > height + 50) {
+                this.posY[i] = -50;
+            } else if (this.direccion[i] === -1 && this.posY[i] < -50) {
+                this.posY[i] = height + 50;
             }
         }
-}
-this.estanDibujadas = true;
-
     }
-
-    
 }
