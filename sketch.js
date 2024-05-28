2904cb5af62f2d54a9baf68113dc68feae49468c
mateodiazw <mateodiazw@gmail.com>
2024-05-28 17:28:01 -0300

    prototipado listo de movimiento de lineas segun agudeza de voz (prototipado con MouseY)

--- sketch.js
+++ sketch.js
@@ -4,21 +4,37 @@ let c;
 // let posY = 0;
 
 function preload() {
-    // Cargar las imágenes
-    for (let i = 0; i <= cantidadVerticales; i++) {
-        lineasVerticales[i] = loadImage("data/vertical" + i + ".png");
-    }
+  // Cargar las imágenes
+  for (let i = 0; i <= cantidadVerticales; i++) {
+    lineasVerticales[i] = loadImage("data/vertical" + i + ".png");
+  }
 }
 
 function setup() {
-    imageMode(CENTER);
-    createCanvas(500, 500);
-    c = new vertical(lineasVerticales);
-    c.inicializar();
-    background(156, 203, 241);
+  imageMode(CENTER);
+  createCanvas(500, 500);
+  c = new vertical(lineasVerticales);
+  c.inicializar();
+  //frameRate(1);
 }
 
 function draw() {
-    c.dibujar();
-    c.actualizar();
+  background(156, 203, 241);
+
+  c.actualizar();
+  c.dibujar();
+
+  //estos textos sirven de ayuda para entender como funciona el prototipo
+  text("mouseY en este cuadrante simula voz aguda", 10, (height/2)-50, 100);
+  text("mouseY en este cuadrante simula voz grave", 10, (height/2)+10, 100);
+
+  //debug
+  line(0, height/2, width, height/2)
+  text("c.posY:" + int(c.posY), 50, 70);
+  text("c.posX:" + int(c.posX), 50, 90);
+  text("c.linea:" + int(c.linea), 50, 110);
+  text("c.calida:" + int(c.calida), 50, 130);
+  text("c.agudeza:" + int(c.agudeza), 50, 150);
+
+
 }
--- vertical.js
+++ vertical.js
@@ -1,52 +1,111 @@
-
 class vertical {
-    constructor(vertical) {
-        this.img = vertical;
-        this.cantidadVerticales = int(random(3, 7)); // Asegurar que es un número entero
-        this.estanDibujadas = false;
-        this.posY = [];
-        this.direccion = [];
-        this.velocidad = 5;
+  constructor(vertical) {
+    this.img = vertical;
+    this.cantidadVerticales = int(random(3, 7)); // Asegurar que es un número entero
+    //this.estanDibujadas = false;
+    this.linea = [];
+    this.posY = [];
+    this.posX = [];
+    this.velocidad = 5;
+    this.tam = 350; // Define el tamaño en el que se dibujan las líneas
+    this.limiteinferior = 400; // Define el limite superior del lienzo para determinar inicializacion en Y de las líneas
+    this.limitesuperior = -150; // Define el limite inferior
+    this.agudeza; // Define la agudeza ( a nivel prorotipo, la determina la posicion del MouseY)
+    this.calida = []; // Este booleano evalua si la linea es o no calida para determinar su direccion en Y
+  }
+
+  inicializar() {
+    // Este método asigna valores para todas las propiedades de las líneas
+    for (let i = 0; i < this.cantidadVerticales; i++) {
+      this.posY.push(int(random(this.limitesuperior * 2, 0)));
+      this.posX.push(100 + i * 75);
+      this.linea.push(int(random(this.img.length))); // Selecciona la linea dentro del arreglo de imagenes
+      this.calida.push(this.linea[i] < 6 ? true : false); // Evalua si la línea es calida o no
+    }
+  }
+
+  dibujar() {
+    for (let i = 0; i < this.cantidadVerticales; i++) {
+      image(
+        this.img[this.linea[i]],
+        this.posX[i],
+        this.posY[i],
+        this.tam,
+        this.tam
+      );
     }
 
-    inicializar() {
-        for (let i = 0; i < this.cantidadVerticales; i++) {
-            this.posY.push(random(-50, height + 50));
-            this.direccion.push(i < 6 ? 1 : -1); // Direcciones: 1 para abajo, -1 para arriba
-        }
-    }
+    // if (!this.estanDibujadas) {
+    // for (let i = 0; i < this.cantidadVerticales; i++) {
+    //     let idx = int(random(this.img.length)); // Índice aleatorio dentro del rango de imágenes
+    //     if (this.img[idx]) {
+    //         let x = 100 + i * 75;
+    //        // let y = this.posY[i]; // Usar i para mantener la posición correcta
+    //         image(this.img[idx], x, this.posY[i], 350, 350);
+    //     }
+    // }
+    // this.estanDibujadas = true;
+    // }
 
-    dibujar() {
-        if (!this.estanDibujadas) {
-            for (let i = 0; i < this.cantidadVerticales; i++) {
-                let idx = int(random(this.img.length)); // Índice aleatorio dentro del rango de imágenes
-                if (this.img[idx]) {
-                    let x = 100 + i * 75;
-                    let y = this.posY[i]; // Usar i para mantener la posición correcta
-                    image(this.img[idx], x, y, 350, 350);
-                }
-            }
-            this.estanDibujadas = true;
-        }
-        // if (!this.estanDibujadas){
-        //     for (let i = 0; i < this.cantidadVerticales; i++) {
-        //         if (this.img[i]) {
-        //             this.pinceladaRandom= int(random(9));
-        //             image(this.img[this.pinceladaRandom], 100 + i * 75 , this.posY[i], 350, 350);
-        //         }
-        //     }
-        // }
-    }
+    // if (!this.estanDibujadas){
+    //     for (let i = 0; i < this.cantidadVerticales; i++) {
+    //         if (this.img[i]) {
+    //             this.pinceladaRandom= int(random(9));
+    //             image(this.img[this.pinceladaRandom], 100 + i * 75 , this.posY[i], 350, 350);
+    //         }
+    //     }
+    // }
+  }
 
-    actualizar() {
-        for (let i = 0; i < this.posY.length; i++) {
-            this.posY[i] += this.velocidad * this.direccion[i];
-            // Reiniciar la posición si la imagen se sale de los límites
-            if (this.direccion[i] === 1 && this.posY[i] > height + 50) {
-                this.posY[i] = -50;
-            } else if (this.direccion[i] === -1 && this.posY[i] < -50) {
-                this.posY[i] = height + 50;
-            }
+  actualizar() {
+    // Prototipo de agudeza de la voz con valor de Mouse Y:
+    // si está por encima de la mitad del lienzo,
+    // sería voz aguda; y si está por debajo, voz grave.
+    this.agudeza = mouseY;
+
+    //  Este for determina la direccion de las lineas segun agudeza de voz
+    for (let i = 0; i < this.posY.length; i++) {
+      if (this.agudeza > height / 2) { // Voz aguda = MouseY en mitad de lienzo hacia arriba
+        // Si la voz es aguda suben los calidos y bajan los frios
+        if (this.calida[i]) {
+          this.posY[i] += this.velocidad; // Suben lineas calidas 
+        } else if (!this.calida[i]) {
+          this.posY[i] -= this.velocidad; // Bajan lineas frias
         }
+      } else if (this.agudeza < height / 2) { // Voz grave = MouseY en mitad de lienzo hacia abajo
+        // Si la voz es grave suben los frios y bajan los calidos
+        if (this.calida[i]) {
+          this.posY[i] -= this.velocidad; // Suben lineas frias
+        } else if (!this.calida[i]) {
+          this.posY[i] += this.velocidad; //Bajan lineas calidas
+        }
+      } else {
+      }
+
+      // Reiniciar la posición si la imagen se sale de los límites
+      if (this.posY[i] > height + this.limiteinferior) {
+        this.posY[i] = this.limitesuperior;
+      } else if (this.posY[i] < this.limitesuperior
+      ) {
+        this.posY[i] = height + this.limiteinferior;
+      }
     }
+  }
+
+  //   for (let i = 0; i < this.posY.length; i++) {
+  //     this.posY[i] += this.velocidad * this.direccion[i];
+  //     // Reiniciar la posición si la imagen se sale de los límites
+  //     if (
+  //       this.direccion[i] === 1 &&
+  //       this.posY[i] > height + this.limiteinferior
+  //     ) {
+  //       this.posY[i] = this.limitesuperior;
+  //     } else if (
+  //       this.direccion[i] === -1 &&
+  //       this.posY[i] < this.limitesuperior
+  //     ) {
+  //       this.posY[i] = height + this.limiteinferior;
+  //     }
+  //   }
+  //}
 }
