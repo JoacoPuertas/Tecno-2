4d0e6682c11a077fe291ad1bd1897fd8ef2ec71b
LaraPuppo <larapuppomugetti@gmail.com>
2024-05-29 23:45:12 -0300

    zenon

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 let lineasVerticales = [];
 let cantidadVerticales = 9;
 let c;
+let cantidadHorizontales = 5;
+let lineasHorizontales = [];
+let m;
 // let posY = 0;
 
 function preload() {
@@ -8,22 +11,28 @@ function preload() {
   for (let i = 0; i <= cantidadVerticales; i++) {
     lineasVerticales[i] = loadImage("data/vertical" + i + ".png");
   }
+  for (let i = 0; i < cantidadHorizontales; i++) {
+    lineasHorizontales[i] = loadImage("data/mancha" + i + ".png")
+  }
+
 }
 
 function setup() {
   imageMode(CENTER);
   createCanvas(500, 500);
   c = new vertical(lineasVerticales);
+  m = new horizontal(lineasHorizontales);
   c.inicializar();
   //frameRate(1);
+
 }
 
 function draw() {
   background(156, 203, 241);
-
+  
   c.actualizar();
   c.dibujar();
-
+  m.dibujar();
   //estos textos sirven de ayuda para entender como funciona el prototipo
   text("mouseY en este cuadrante simula voz aguda", 10, (height/2)-50, 100);
   text("mouseY en este cuadrante simula voz grave", 10, (height/2)+10, 100);
